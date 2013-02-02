02edc0ce5c69d2960d52c18308411a4ec691f4dc
siriuzwhite <siriuz@gmx.net>
2013-02-02 04:21:42 +0100

    added options form logic

--- Options.Designer.cs
+++ Options.Designer.cs
@@ -36,6 +36,9 @@
             this.btn_apply = new System.Windows.Forms.Button();
             this.btn_cancel = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cb_sounds = new System.Windows.Forms.CheckBox();
+            this.num_opacity = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
             this.rb_windowTray = new System.Windows.Forms.RadioButton();
             this.rb_windowTop = new System.Windows.Forms.RadioButton();
             this.rb_windowNormal = new System.Windows.Forms.RadioButton();
@@ -51,7 +54,7 @@
             this.lnk_nma = new System.Windows.Forms.LinkLabel();
             this.lbl_prowl = new System.Windows.Forms.Label();
             this.cb_prowl = new System.Windows.Forms.CheckBox();
-            this.tbx_nma = new System.Windows.Forms.TextBox();
+            this.tb_nma = new System.Windows.Forms.TextBox();
             this.lbl_nma = new System.Windows.Forms.Label();
             this.cb_nma = new System.Windows.Forms.CheckBox();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
@@ -62,13 +65,10 @@
             this.tb_mailTo = new System.Windows.Forms.TextBox();
             this.lbl_mailAddress = new System.Windows.Forms.Label();
             this.cb_mail = new System.Windows.Forms.CheckBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.num_opacity = new System.Windows.Forms.NumericUpDown();
-            this.cb_sounds = new System.Windows.Forms.CheckBox();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.num_opacity)).BeginInit();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.num_opacity)).BeginInit();
             this.SuspendLayout();
             // 
             // cb_autoJoin
@@ -103,15 +103,18 @@
             this.btn_ok.TabIndex = 10;
             this.btn_ok.Text = "Ok";
             this.btn_ok.UseVisualStyleBackColor = true;
+            this.btn_ok.Click += new System.EventHandler(this.btn_ok_Click);
             // 
             // btn_apply
             // 
+            this.btn_apply.Enabled = false;
             this.btn_apply.Location = new System.Drawing.Point(475, 386);
             this.btn_apply.Name = "btn_apply";
             this.btn_apply.Size = new System.Drawing.Size(75, 23);
             this.btn_apply.TabIndex = 11;
             this.btn_apply.Text = "Apply";
             this.btn_apply.UseVisualStyleBackColor = true;
+            this.btn_apply.Click += new System.EventHandler(this.btn_apply_Click);
             // 
             // btn_cancel
             // 
@@ -121,6 +124,7 @@
             this.btn_cancel.TabIndex = 12;
             this.btn_cancel.Text = "Cancel";
             this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
             // 
             // groupBox1
             // 
@@ -143,6 +147,39 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "General options";
             // 
+            // cb_sounds
+            // 
+            this.cb_sounds.AutoSize = true;
+            this.cb_sounds.Checked = true;
+            this.cb_sounds.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cb_sounds.Location = new System.Drawing.Point(16, 88);
+            this.cb_sounds.Name = "cb_sounds";
+            this.cb_sounds.Size = new System.Drawing.Size(96, 17);
+            this.cb_sounds.TabIndex = 19;
+            this.cb_sounds.Text = "Enable sounds";
+            this.cb_sounds.UseVisualStyleBackColor = true;
+            // 
+            // num_opacity
+            // 
+            this.num_opacity.Location = new System.Drawing.Point(26, 281);
+            this.num_opacity.Name = "num_opacity";
+            this.num_opacity.Size = new System.Drawing.Size(85, 20);
+            this.num_opacity.TabIndex = 18;
+            this.num_opacity.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 265);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Opacity:";
+            // 
             // rb_windowTray
             // 
             this.rb_windowTray.AutoSize = true;
@@ -150,13 +187,13 @@
             this.rb_windowTray.Name = "rb_windowTray";
             this.rb_windowTray.Size = new System.Drawing.Size(88, 17);
             this.rb_windowTray.TabIndex = 16;
-            this.rb_windowTray.TabStop = true;
             this.rb_windowTray.Text = "always in tray";
             this.rb_windowTray.UseVisualStyleBackColor = true;
             // 
             // rb_windowTop
             // 
             this.rb_windowTop.AutoSize = true;
+            this.rb_windowTop.Checked = true;
             this.rb_windowTop.Location = new System.Drawing.Point(26, 216);
             this.rb_windowTop.Name = "rb_windowTop";
             this.rb_windowTop.Size = new System.Drawing.Size(90, 17);
@@ -172,7 +209,6 @@
             this.rb_windowNormal.Name = "rb_windowNormal";
             this.rb_windowNormal.Size = new System.Drawing.Size(56, 17);
             this.rb_windowNormal.TabIndex = 14;
-            this.rb_windowNormal.TabStop = true;
             this.rb_windowNormal.Text = "normal";
             this.rb_windowNormal.UseVisualStyleBackColor = true;
             // 
@@ -188,6 +224,8 @@
             // cb_lock
             // 
             this.cb_lock.AutoSize = true;
+            this.cb_lock.Checked = true;
+            this.cb_lock.CheckState = System.Windows.Forms.CheckState.Checked;
             this.cb_lock.Location = new System.Drawing.Point(16, 307);
             this.cb_lock.Name = "cb_lock";
             this.cb_lock.Size = new System.Drawing.Size(143, 17);
@@ -198,6 +236,8 @@
             // cb_balloonTips
             // 
             this.cb_balloonTips.AutoSize = true;
+            this.cb_balloonTips.Checked = true;
+            this.cb_balloonTips.CheckState = System.Windows.Forms.CheckState.Checked;
             this.cb_balloonTips.Location = new System.Drawing.Point(16, 135);
             this.cb_balloonTips.Name = "cb_balloonTips";
             this.cb_balloonTips.Size = new System.Drawing.Size(115, 17);
@@ -208,6 +248,8 @@
             // cb_desktopNotification
             // 
             this.cb_desktopNotification.AutoSize = true;
+            this.cb_desktopNotification.Checked = true;
+            this.cb_desktopNotification.CheckState = System.Windows.Forms.CheckState.Checked;
             this.cb_desktopNotification.Location = new System.Drawing.Point(16, 111);
             this.cb_desktopNotification.Name = "cb_desktopNotification";
             this.cb_desktopNotification.Size = new System.Drawing.Size(165, 17);
@@ -224,7 +266,7 @@
             this.groupBox2.Controls.Add(this.lnk_nma);
             this.groupBox2.Controls.Add(this.lbl_prowl);
             this.groupBox2.Controls.Add(this.cb_prowl);
-            this.groupBox2.Controls.Add(this.tbx_nma);
+            this.groupBox2.Controls.Add(this.tb_nma);
             this.groupBox2.Controls.Add(this.lbl_nma);
             this.groupBox2.Controls.Add(this.cb_nma);
             this.groupBox2.Location = new System.Drawing.Point(228, 12);
@@ -236,6 +278,7 @@
             // 
             // btn_prowlTest
             // 
+            this.btn_prowlTest.Enabled = false;
             this.btn_prowlTest.Location = new System.Drawing.Point(224, 144);
             this.btn_prowlTest.Name = "btn_prowlTest";
             this.btn_prowlTest.Size = new System.Drawing.Size(75, 23);
@@ -245,6 +288,7 @@
             // 
             // btn_nmaTest
             // 
+            this.btn_nmaTest.Enabled = false;
             this.btn_nmaTest.Location = new System.Drawing.Point(223, 53);
             this.btn_nmaTest.Name = "btn_nmaTest";
             this.btn_nmaTest.Size = new System.Drawing.Size(75, 23);
@@ -255,6 +299,7 @@
             // lnk_prowl
             // 
             this.lnk_prowl.AutoSize = true;
+            this.lnk_prowl.Enabled = false;
             this.lnk_prowl.Location = new System.Drawing.Point(41, 175);
             this.lnk_prowl.Name = "lnk_prowl";
             this.lnk_prowl.Size = new System.Drawing.Size(102, 13);
@@ -264,6 +309,7 @@
             // 
             // tb_prowl
             // 
+            this.tb_prowl.Enabled = false;
             this.tb_prowl.Location = new System.Drawing.Point(38, 148);
             this.tb_prowl.Name = "tb_prowl";
             this.tb_prowl.Size = new System.Drawing.Size(179, 20);
@@ -272,6 +318,7 @@
             // lnk_nma
             // 
             this.lnk_nma.AutoSize = true;
+            this.lnk_nma.Enabled = false;
             this.lnk_nma.Location = new System.Drawing.Point(38, 82);
             this.lnk_nma.Name = "lnk_nma";
             this.lnk_nma.Size = new System.Drawing.Size(100, 13);
@@ -282,6 +329,7 @@
             // lbl_prowl
             // 
             this.lbl_prowl.AutoSize = true;
+            this.lbl_prowl.Enabled = false;
             this.lbl_prowl.Location = new System.Drawing.Point(35, 131);
             this.lbl_prowl.Name = "lbl_prowl";
             this.lbl_prowl.Size = new System.Drawing.Size(101, 13);
@@ -297,17 +345,20 @@
             this.cb_prowl.TabIndex = 3;
             this.cb_prowl.Text = "Use iOS push notifications (Prowl)";
             this.cb_prowl.UseVisualStyleBackColor = true;
+            this.cb_prowl.CheckedChanged += new System.EventHandler(this.cb_prowl_CheckedChanged);
             // 
-            // tbx_nma
+            // tb_nma
             // 
-            this.tbx_nma.Location = new System.Drawing.Point(38, 55);
-            this.tbx_nma.Name = "tbx_nma";
-            this.tbx_nma.Size = new System.Drawing.Size(179, 20);
-            this.tbx_nma.TabIndex = 2;
+            this.tb_nma.Enabled = false;
+            this.tb_nma.Location = new System.Drawing.Point(38, 55);
+            this.tb_nma.Name = "tb_nma";
+            this.tb_nma.Size = new System.Drawing.Size(179, 20);
+            this.tb_nma.TabIndex = 2;
             // 
             // lbl_nma
             // 
             this.lbl_nma.AutoSize = true;
+            this.lbl_nma.Enabled = false;
             this.lbl_nma.Location = new System.Drawing.Point(35, 39);
             this.lbl_nma.Name = "lbl_nma";
             this.lbl_nma.Size = new System.Drawing.Size(99, 13);
@@ -323,6 +374,7 @@
             this.cb_nma.TabIndex = 0;
             this.cb_nma.Text = "Use Android push notifications (NMA)";
             this.cb_nma.UseVisualStyleBackColor = true;
+            this.cb_nma.CheckedChanged += new System.EventHandler(this.cb_nma_CheckedChanged);
             // 
             // contextMenuStrip1
             // 
@@ -346,6 +398,7 @@
             // 
             // btn_mailTest
             // 
+            this.btn_mailTest.Enabled = false;
             this.btn_mailTest.Location = new System.Drawing.Point(223, 103);
             this.btn_mailTest.Name = "btn_mailTest";
             this.btn_mailTest.Size = new System.Drawing.Size(75, 23);
@@ -355,6 +408,7 @@
             // 
             // tb_mailSMTP
             // 
+            this.tb_mailSMTP.Enabled = false;
             this.tb_mailSMTP.Location = new System.Drawing.Point(38, 106);
             this.tb_mailSMTP.Name = "tb_mailSMTP";
             this.tb_mailSMTP.Size = new System.Drawing.Size(179, 20);
@@ -363,6 +417,7 @@
             // lbl_mailSMTP
             // 
             this.lbl_mailSMTP.AutoSize = true;
+            this.lbl_mailSMTP.Enabled = false;
             this.lbl_mailSMTP.Location = new System.Drawing.Point(38, 90);
             this.lbl_mailSMTP.Name = "lbl_mailSMTP";
             this.lbl_mailSMTP.Size = new System.Drawing.Size(133, 13);
@@ -371,6 +426,7 @@
             // 
             // tb_mailTo
             // 
+            this.tb_mailTo.Enabled = false;
             this.tb_mailTo.Location = new System.Drawing.Point(38, 58);
             this.tb_mailTo.Name = "tb_mailTo";
             this.tb_mailTo.Size = new System.Drawing.Size(179, 20);
@@ -379,6 +435,7 @@
             // lbl_mailAddress
             // 
             this.lbl_mailAddress.AutoSize = true;
+            this.lbl_mailAddress.Enabled = false;
             this.lbl_mailAddress.Location = new System.Drawing.Point(35, 42);
             this.lbl_mailAddress.Name = "lbl_mailAddress";
             this.lbl_mailAddress.Size = new System.Drawing.Size(69, 13);
@@ -394,37 +451,7 @@
             this.cb_mail.TabIndex = 0;
             this.cb_mail.Text = "Send mail notification";
             this.cb_mail.UseVisualStyleBackColor = true;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(8, 265);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 13);
-            this.label2.TabIndex = 17;
-            this.label2.Text = "Opacity:";
-            // 
-            // num_opacity
-            // 
-            this.num_opacity.Location = new System.Drawing.Point(26, 281);
-            this.num_opacity.Name = "num_opacity";
-            this.num_opacity.Size = new System.Drawing.Size(56, 20);
-            this.num_opacity.TabIndex = 18;
-            this.num_opacity.Value = new decimal(new int[] {
-            100,
-            0,
-            0,
-            0});
-            // 
-            // cb_sounds
-            // 
-            this.cb_sounds.AutoSize = true;
-            this.cb_sounds.Location = new System.Drawing.Point(16, 88);
-            this.cb_sounds.Name = "cb_sounds";
-            this.cb_sounds.Size = new System.Drawing.Size(96, 17);
-            this.cb_sounds.TabIndex = 19;
-            this.cb_sounds.Text = "Enable sounds";
-            this.cb_sounds.UseVisualStyleBackColor = true;
+            this.cb_mail.CheckedChanged += new System.EventHandler(this.cb_mail_CheckedChanged);
             // 
             // Options
             // 
@@ -440,13 +467,14 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Options";
             this.Text = "Dungeon Teller - Options";
+            this.Load += new System.EventHandler(this.Options_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.num_opacity)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.num_opacity)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -460,7 +488,7 @@
         private System.Windows.Forms.Button btn_cancel;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.TextBox tbx_nma;
+        private System.Windows.Forms.TextBox tb_nma;
         private System.Windows.Forms.Label lbl_nma;
         private System.Windows.Forms.CheckBox cb_nma;
         private System.Windows.Forms.CheckBox cb_prowl;
--- Options.cs
+++ Options.cs
@@ -15,5 +15,78 @@ namespace Dungeon_Teller
         {
             InitializeComponent();
         }
+
+        private void Options_Load(object sender, EventArgs e)
+        {
+
+            if (cb_nma.Checked == true) toogleNMA();
+
+        }
+
+
+        private void toogleNMA()
+        {
+            tb_nma.Enabled = !tb_nma.Enabled;
+            lnk_nma.Enabled = !lnk_nma.Enabled;
+            lbl_nma.Enabled = !lbl_nma.Enabled;
+            btn_nmaTest.Enabled = !btn_nmaTest.Enabled;
+        }
+
+        private void toogleProwl()
+        {
+            tb_prowl.Enabled = !tb_prowl.Enabled;
+            lnk_prowl.Enabled = !lnk_prowl.Enabled;
+            lbl_prowl.Enabled = !lbl_prowl.Enabled;
+            btn_prowlTest.Enabled = !btn_prowlTest.Enabled;
+        }
+
+        private void toogleMail()
+        {
+            lbl_mailSMTP.Enabled = !lbl_mailSMTP.Enabled;
+            lbl_mailAddress.Enabled = !lbl_mailAddress.Enabled;
+            tb_mailTo.Enabled = !tb_mailTo.Enabled;
+            tb_mailSMTP.Enabled = !tb_mailSMTP.Enabled;
+            btn_mailTest.Enabled = !btn_mailTest.Enabled;
+        }
+
+        private void cb_nma_CheckedChanged(object sender, EventArgs e)
+        {
+            toogleNMA();
+        }
+
+        
+        private void cb_prowl_CheckedChanged(object sender, EventArgs e)
+        {
+            toogleProwl();
+        }
+
+        private void cb_mail_CheckedChanged(object sender, EventArgs e)
+        {
+            toogleMail();
+        }
+
+        private void btn_cancel_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void applySettings()
+        {
+
+        }
+
+        private void btn_ok_Click(object sender, EventArgs e)
+        {
+            applySettings();
+            this.Hide();
+        }
+
+        private void btn_apply_Click(object sender, EventArgs e)
+        {
+            btn_apply.Enabled = false;
+            applySettings();
+        }
+
+
     }
 }
